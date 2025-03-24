ed5fd1494a521f48b3cb3226d0d034e53453b8cc
Prashant Bhargava <118739991+bhargava-prashant@users.noreply.github.com>
2025-03-24 19:47:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,14 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 import io
 import hashlib  # For password hashing
+
+
 
 # Initialize Streamlit app
 st.set_page_config(page_title="MinAI", page_icon="Untitled design (4).png")
@@ -121,7 +124,7 @@ def get_conversational_chain():
     Question: \n{question}\n
     Answer:
     """
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+    model = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.3)
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     return chain
